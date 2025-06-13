8cb76b36f8dc46419ad56e4a8babe660758c64cc
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-06-13 07:03:05 +0530

    refactor: optimize Pascal's Triangle calculation and rename methods for clarity

--- Day1_Array/2_PascalsTriangle/PascalsTriangleVar3_EntireTriangle.cpp
+++ Day1_Array/2_PascalsTriangle/PascalsTriangleVar3_EntireTriangle.cpp
@@ -1,60 +1,37 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Solution
 {
-  long long factorial(int n)
+  vector<int> calculatePascalRow(int numRow)
   {
-    long long result = 1;
-    for (int i = 1; i <= n; i++)
+    long long val = 1;
+    vector<int> ans;
+    ans.push_back(val);
+    for (int i = 1; i < numRow; i++)
     {
-      result *= i;
-    }
-    return result;
-  }
-  int nCr(int n, int r)
-  {
-    int nfact = factorial(n);
-    int rfact = factorial(r);
-    int nrfact = factorial(n - r);
-    return nfact / (rfact * nrfact);
-  }
-  vector<int>
-  calculatePascalRow(int numRow)
-  {
-    vector<int> pascalRow;
-    pascalRow.push_back(1);
-    if (numRow >= 2)
-    {
-      for (int i = 2; i < numRow; i++)
-      {
-        long long pascalElement = nCr(numRow - 1, i - 1);
-        pascalRow.push_back(pascalElement);
-      }
-      pascalRow.push_back(1);
-    }
-    return pascalRow;
-  }
-
-public:
-  vector<vector<int>> pascalTriangleEntire(int numRows)
-  {
-    vector<vector<int>> ans;
-    for (int i = 1; i <= numRows; i++)
-    {
-      vector<int> rowAns = calculatePascalRow(i);
-      ans.push_back(rowAns);
+      val = val * (numRow - i) / i;
+      ans.push_back(val);
     }
     return ans;
   }
-};
 
+public:
+  vector<vector<int>> generate(int numRows)
+  {
+    vector<vector<int>> result;
+    for (int i = 1; i <= numRows; i++)
+    {
+      result.push_back(calculatePascalRow(i));
+    }
+    return result;
+  }
+};
 int main()
 {
   Solution s = Solution();
   int numRows = 5;
-  vector<vector<int>> ans = s.pascalTriangleEntire(numRows);
+  vector<vector<int>> ans = s.generate(numRows);
   for (auto i : ans)
   {
     for (auto j : i)
