ddb94c5f79af094d958afb94e82046d142de16f8
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-08-28 20:55:48 +0530

    unoptimized find peak element

---
+++ A2Z SHEET/4_Binary Search/Binary Search 1D Array/13_Find Peak Element/unoptimized_peak_element.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  void solution()
+  {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
+    int peakIndex = -1;
+    if(arr.size()==1)peakIndex=0;
+    if(arr[0]>arr[1])peakIndex = 0;
+    if(arr[arr.size()-1] > arr[arr.size()-2])peakIndex=arr.size()-1;
+    if(peakIndex != -1){
+      cout<<"Peak Index: "<<peakIndex;return;
+    }
+
+    for(int i=1;i<arr.size()-1;i++){
+      if(arr[i] > arr[i-1] && arr[i]>arr[i+1]){
+        peakIndex=i;
+        break;
+      }
+    }
+    cout<<"Peak Index: "<<peakIndex;
+  }
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << endl;
+  Solution sol = Solution();
+  sol.solution();
+  cout << endl;
+}
