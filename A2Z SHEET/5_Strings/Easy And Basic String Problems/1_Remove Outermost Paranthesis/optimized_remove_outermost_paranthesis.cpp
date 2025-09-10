7275d13eb95a00502b23ea15472f613ce738309f
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-09-10 17:42:42 +0530

    optimized remove outermost parnathesis

---
+++ A2Z SHEET/5_Strings/Easy And Basic String Problems/1_Remove Outermost Paranthesis/optimized_remove_outermost_paranthesis.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+  void solution()
+  {
+    string str = "(()())(())(()(()))";
+    string ans = "";
+    int paranthesisCount=0;
+
+    for (char &ch : str)
+    {
+      if (ch == '(')
+      {
+        if(paranthesisCount != 0) ans+="(";
+        paranthesisCount++;
+      }
+      else
+      {
+        if(paranthesisCount > 1) ans+=")";
+        paranthesisCount--;
+      }
+    }
+    cout << "Removed Outer Parenthesis: " << ans;
+  }
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << endl;
+  Solution sol = Solution();
+  sol.solution();
+  cout << endl;
+}
