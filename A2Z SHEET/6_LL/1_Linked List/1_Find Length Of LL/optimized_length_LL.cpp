567051d9c9be284c54d6673ef22acd5fb5955abc
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-09-24 16:20:53 +0530

    optimized length LL

---
+++ A2Z SHEET/6_LL/1_Linked List/1_Find Length Of LL/optimized_length_LL.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+template <typename T>
+class Node
+{
+public:
+  T data;
+  Node<T> *next;
+  Node(T data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+class Solution
+{
+  Node<int> *convertArrToLL(vector<int> &arr)
+  {
+    if (arr.size() == 0)
+    {
+      return nullptr;
+    }
+
+    Node<int> *head = new Node<int>(arr[0]);
+    Node<int> *temp = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+      temp->next = new Node<int>(arr[i]);
+      temp = temp->next;
+    }
+    return head;
+  }
+  void printLL(Node<int> *&head)
+  {
+    if (head == nullptr)
+    {
+      return;
+    }
+
+    Node<int> *temp = head;
+    while (temp != nullptr)
+    {
+      cout << temp->data << ", ";
+      temp = temp->next;
+    }
+    cout << "\n";
+    ;
+  }
+
+  int findLengthLL(Node<int> *head)
+  {
+    if (head == nullptr)
+    {
+      return 0;
+    }
+    Node<int> *temp = head;
+    int cnt = 0;
+    while (temp != nullptr)
+    {
+      cnt++;
+      temp = temp->next;
+    }
+    return cnt;
+  }
+
+public:
+  void
+  solution()
+  {
+    vector<int> arr = {1, 4, 2, 6, 88};
+    Node<int> *head = convertArrToLL(arr);
+    printLL(head);
+    int count = findLengthLL(head);
+    cout << "Length Of LL: " << count;
+  }
+};
+
+int main()
+{
+  cout << "\n";
+  Solution sol = Solution();
+  sol.solution();
+  cout << "\n";
+}
