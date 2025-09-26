737fa8a189195df0d1b0d50228ffc64c40bc063f
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-09-26 16:55:10 +0530

    optimized reverse LL Recursive

---
+++ A2Z SHEET/6_LL/3_Medium_LL/2_Reverse LL/optimized_Reverse_LL_recursive.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+template <typename T>
+class Node
+{
+public:
+  T data;
+  Node<T> *next;
+  Node(T data)
+  {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+class Solution
+{
+  Node<int> *convertArrToLL(vector<int> &arr)
+  {
+    if (arr.size() == 0)
+    {
+      return nullptr;
+    }
+
+    Node<int> *head = new Node<int>(arr[0]);
+    Node<int> *temp = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+      temp->next = new Node<int>(arr[i]);
+      temp = temp->next;
+    }
+    return head;
+  }
+  void printLL(Node<int> *&head)
+  {
+    if (head == nullptr)
+    {
+      return;
+    }
+
+    Node<int> *temp = head;
+    while (temp != nullptr)
+    {
+      cout << temp->data << ", ";
+      temp = temp->next;
+    }
+    cout << "\n";
+  }
+  Node<int> *reverseLL(Node<int> *&head)
+  {
+    // BASE CASE
+    if (head == nullptr || head->next == nullptr)
+    {
+      return head;
+    }
+    Node<int>*reversedHead = reverseLL(head->next);
+    Node<int>* nextNode = head->next;
+    nextNode->next = head;
+    head->next = nullptr;
+    return reversedHead;
+  }
+
+public:
+  void solution()
+  {
+    vector<int> arr = {1, 4, 2, 6, 88};
+    Node<int> *head = convertArrToLL(arr);
+    head = reverseLL(head);
+    printLL(head);
+  }
+};
+
+int main()
+{
+  cout << "\n";
+  Solution sol = Solution();
+  sol.solution();
+  cout << "\n";
+}
